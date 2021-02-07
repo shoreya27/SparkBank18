7c8cce26e04f575ffec45a19bd67a7c3342219cb
shoreya27 <gupta.shoreya2707@gmail.com>
2021-02-07 11:41:28 +0530

    index page

---
+++ spark18bank/views.py
@@ -0,0 +1,5 @@
+from django.http.response import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hi!! Thanks For choosing SparkBank18 .\n This is a small Assignment Project. Continue to enjoy Utility Apis provided in Github Repo....")
